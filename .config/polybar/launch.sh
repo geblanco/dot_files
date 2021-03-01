55924eead513b4b5e457f4f946b513ce178c5ef2
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2021-03-01 12:02:58 +0100

    Add polybar logrotate

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -15,6 +15,13 @@ screens=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | sort -u)
 
 today=$(date +%Y-%m-%d)
 
+echo "Cleaning log"
+if [[ "$(ls -t | awk '{ if (length($NF) > 4) print }' | wc -l)" -gt 6 ]]; then
+  for file in $(ls -t | awk '{ if (length($NF) > 4) print }' | tail -n 6); do
+    rm "$file"
+  done
+fi
+
 # Launch bar1 and bar2
 for screen in $screens; do
   MONITOR=$screen polybar main -c ~/.config/polybar/working -l info 1>&2 2>~/.config/polybar/log/$today-app.log &
